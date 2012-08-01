9e64f9929f074747d62bd07b5352b2a39e2e3de0
Gabor Szarnyas <szarnyasg@gmail.com>
2012-08-01 11:35:47 +0200

    cmap package, relay php script

---
+++ pdf_relay/pdf.php
@@ -0,0 +1,13 @@
+<?
+  function flushPDF($url, $filename) {
+    header('Content-type: application/pdf');
+    header('Content-Disposition: inline; filename="'.$filename.'"');
+    readfile($url);
+  }
+  
+  $document = $_GET["document"];
+  $url = 'https://github.com/szarnyasg/BUTE-Databases-Course-Materials/blob/master/' . $document . '/' . $document . '.pdf?raw=true';
+  $filename = $document . ".pdf";
+  
+  flushPDF($url, $filename);
+?>
